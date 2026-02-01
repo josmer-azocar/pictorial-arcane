75844c0a326e22eb41ed48163ebc62d3895e5f76
Lisxstar <lxxdvc@gmail.com>
2026-01-31 20:24:25 -0400

    Lógica de validación del login

--- frontend/src/pages/auth/MainAuth.jsx
+++ frontend/src/pages/auth/MainAuth.jsx
@@ -9,12 +9,19 @@ function MainAuth() {
 
     return (
         <section className='auth-form'>
-            <div id='auth-buttons'>
-                <button onClick={()=> setAuthPage(true)}>Login</button>
-                <button onClick={()=> setAuthPage(false)}>Sign Up</button>
-            </div>
-
-            {isLogin? <Login/> : <Sign/>}            
+            <section id='log-click'>
+                <div id='auth-buttons'>
+                    <button onClick={()=> setAuthPage(true)}>Login</button>
+                    <button onClick={()=> setAuthPage(false)}>Sign Up</button>                   
+                </div>
+                {isLogin? <Login/> : <Sign/>}
+            </section>
+            <section id='auth-welcome'>  
+                <div id='text-welcome'>
+                    <p>¡Bienvenido!</p><br/>
+                    <p>Disfuta de nuestra colección de artistas y adquiere piezas únicas al formar parte de nuestra comunidad</p>
+                </div>  
+            </section>
         </section>
     );
 }
